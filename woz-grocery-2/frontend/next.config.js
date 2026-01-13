d516f4e79126aa6155fdb7ad903d58c43fc16042
bernicejywong <bernicejywong@gmail.com>
2026-01-13 09:05:15 -0800

    Update next.config.js

--- woz-grocery-2/frontend/next.config.js
+++ woz-grocery-2/frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+ 
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+ 
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
 };
-
+ 
 module.exports = nextConfig;
